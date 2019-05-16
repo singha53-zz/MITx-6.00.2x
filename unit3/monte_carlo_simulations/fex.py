e3a4a745f47f7b9ff5b1fb7647e4b07945210e22
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2019-05-16 13:40:58 -0400

    add monte_carlo_simulation fex

---
+++ unit3/monte_carlo_simulations/fex.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 16 11:40:03 2019
+
+@author: asingh
+"""
+
+# =============================================================================
+# Exercise 1-1
+# 1/1 point (graded)
+# Suppose we have an experiment. We toss a coin  times. Each time we collect results from a sample of size  and compute this sample's mean  and standard deviation . This experiment has an underlying distribution with mean  and standard deviation .
+# 
+# Which of the following does the Central Limit Theorem (CLT) guarantee (for large enough  and ):
+# 
+# 
+# The sample means will be approximately normally distributed. correct
+# The sample means will have a mean close to the mean of the original distribution . correct
+# The sample means will have a variance close to the variance of the original distribution divided by the sample size . correct
+# correct
+# Submit
+# =============================================================================
